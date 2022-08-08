a211bf4f124e21089b0b1391d690062875600d2c
Laurids Hieden <realjupi1001@gmail.com>
2022-08-08 12:53:09 +0200

    Add alt text

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => (
       <button className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="" />
+      <img src={images.welcome} alt="Welcome" />
     </div>
   </div>
 );
